bfad5f6bcb4b4b45bdcc7d2df552ea17e71db2c8
Shubh0408 <blooddesire4@gmail.com>
2023-08-17 13:22:30 +0530

    drpdown

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import { useTranslation } from "react-i18next"
 
 
 function App() {
-  const [t,i18n] = useTranslation("global");
-  const handleChangeLanguage = (lang: string) => {
-    i18n.changeLanguage(lang);
-};
-
+  const [t, i18n] = useTranslation("global");
+  const handleChangeLanguage = (value) => {
+    i18n.changeLanguage(value);
+  };
+  const getValue = () => {
+    var select = document.getElementById('language');
+    var value = select.options[select.selectedIndex].value;
+    handleChangeLanguage(value);
+    // alert(value);
+  }
   return (
     <div className="App">
-      <button onClick={() => handleChangeLanguage("en")}>EN</button>
-      <button onClick={() => handleChangeLanguage("hi")}>HI</button>
+      {/* <button onClick={() => handleChangeLanguage("en")}>EN</button>
+      <button onClick={() => handleChangeLanguage("hi")}>HI</button> */}
 
-      <select>
-        <option defaultChecked disabled>Lang</option>
+      <select id="language" onChange={() => getValue()}>
+        <option selected disabled>Lang</option>
         <option value="en">En</option>
         <option value="hi"> Hi</option>
       </select>
+
       <p>{t("header.mainline")} </p>
       <Home />
       <Header />
